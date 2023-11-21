94b5d353cb22df7d1533dc29fde9fe2a13964838
Julia Glinka <julia.w.glinka@gmail.com>
2023-11-21 23:01:34 +0100

    Add states currentColor and currentSize

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,8 +2,15 @@ import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-const Product = ({title, basePrice}) => {
+const Product = ({title, basePrice, colors, sizes}) => {
+
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0].name);
+
+  console.log(currentColor, currentSize);
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
